2c3be263a9e251cb8b27ecc1ef701cf949c4100e
Marcin_Chudzik <marcinchudzik.juniordev@gmail.com>
2022-07-29 23:45:34 +0200

    Comment form in CSS/JS box modal

--- Blog/myblog/blog/forms.py
+++ Blog/myblog/blog/forms.py
@@ -13,3 +13,15 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'email', 'body')
+
+    name = forms.CharField(
+        label='Name',
+        widget=forms.TextInput(attrs={'placeholder': 'Your name..'}))
+    email = forms.EmailField(
+        label='E-mail',
+        widget=forms.EmailInput(attrs={'placeholder': 'mail@mail.com'})
+    )
+    body = forms.CharField(
+        label='Comment',
+        widget=forms.Textarea(attrs={'placeholder': 'comment content..'})
+    )
